64beedf42a3f94b05964eadfe55f526cce769c37
bradshumaker <shumakerb@clarke.k12.va.us>
2017-11-06 22:20:09 -0500

    Fixed alot of problems with more to go. Not sure what I was doing.

--- textfile_based/engine.py
+++ textfile_based/engine.py
@@ -1,10 +1,9 @@
 #!/usr/bin/python2
-# Author is Moses Arocha
-# Contributions: Brad Shumaker
-
+# Authors: Moses Arocha
+# Brad Shumaker
 
 #shell=true makes the program vulnerable to 'shell injection'. kinda cool, right?
- 
+
 import os
 import pwd
 import re
@@ -21,6 +20,16 @@ reportLocation = './'
 score = 0
 #points = []
 
+
+def readFile(file):
+   if os.path.isfile(file):
+      pro = subprocess.Popen("cat "+file, shell=True, stdout=subprocess.PIPE)
+      display = pro.stdout.read()
+      pro.stdout.close()
+      pro.wait()
+      return display
+   else:
+      errorWrite('File not found: '+file)
 
 def errorWrite(msg):
    if os.path.isfile("Error_log.txt"):
@@ -66,9 +75,10 @@ def checkComplete(notifytxt): #Prevent Duplicate Prompts
    else:
       f = open(reportLocation+'Score_Report.html', 'w+')
       win_prompt(notifytxt)
+#######################################Engine_Code_Above
+#####MODULES_BELOW
 
-
-def schedule_cron(user, taskstring):
+def schedule_cron(user, taskstring): #fix
    if os.path.isfile("/var/spool/cron/crontabs/"+user): #does the user have a cronjob?
       pro = subprocess.Popen("crontab -l -u"+user+" | grep \""+taskstring+"\"", shell=True, stdout=subprocess.PIPE)
       display = pro.stdout.read()
@@ -79,7 +89,6 @@ def schedule_cron(user, taskstring):
       errorWrite('Error: '+user+' does not have a cron task. no points will be awarded')
 
 
-
 def program_remove(program):
    pro = subprocess.Popen("dpkg -l | grep " +program, shell=True, stdout=subprocess.PIPE)
    display = pro.stdout.read()
@@ -88,76 +97,69 @@ def program_remove(program):
    if not display:
       checkComplete('Program '+program+' removed')
 
-
+###
 def program_respos(topic,respository):
-   pro = subprocess.Popen("cat /etc/apt/sources.list", shell=True, stdout=subprocess.PIPE)
-   display = pro.stdout.read()
-   pro.stdout.close()
-   pro.wait()
+   display = readFile("/etc/apt/sources.list")
    if respository in display:
       checkComplete('Respository '+topic+' Added To Debian Package Lists')
-
+###
 def program_autoupdates():
-   if os.path.isfile("/etc/apt/apt.conf.d/20auto-upgrades"):
+   display = readFile("/etc/apt/apt.conf.d/20auto-upgrades")
+   if 'Unattended-Upgrade "1"' in display and 'Update-Package-Lists "1"' in display:
       checkComplete('Automatic Updates Applied')
 
-
+###?
 def program_kernel(kVersion, kMajorRev, kMinRev): #Pass the minimum kernel version to get points
-   pro = subprocess.Popen("uname -r | cut -d- -f1", shell=True, stdout=subprocess.PIPE)
+   pro = subprocess.Popen("uname -r", shell=True, stdout=subprocess.PIPE)
    display = pro.stdout.read()
    pro.stdout.close()
+   display = display.split('.') #[0] = 4, [1]=4 [2]=0-genimg
+   sysMinRev = display[2].split('-') #sysMinRev=0, cleans above last item
    if (display[0] >= kVersion):
-      if (display[2] > kMajorRev) or (display[2] == kMajorRev and display[4:] > kMinRev): 
+      if (display[1] > kMajorRev) or (display[1] == kMajorRev and sysMinRev[0] > kMinRev):
          checkComplete('System Kernel Upgraded')
 
-
+###
 def user_passwd(user,hash):
-   pro = subprocess.Popen("cat /etc/shadow | grep "+user, shell=True, stdout=subprocess.PIPE)
-   display = pro.stdout.read()
-   pro.stdout.close()
-   pro.wait()
-   if user in display: #no points for deleting valid users!
-      if hash not in display:
-         checkComplete('Changed '+user+' Password')
+   display = readFile('/etc/shadow')
+   if user in display and hash not in display:
+       checkComplete('Changed '+user+' Password')
    #else:
       #remove points, coming soon...
 
-
-def user_hiddenroot():
-   pro = subprocess.Popen("cat /etc/passwd | grep -v root | grep :0:0:", shell=True, stdout=subprocess.PIPE)
-   display = pro.stdout.read()
-   pro.wait()
-   if not display:
-      checkComplete('Hidden Root User Removed')
-
-
-def user_remove(badUser): #renamed _remove because it checks removal not existance. 
-   pro = subprocess.Popen("cat /etc/pam.d/common-auth", shell=True, stdout=subprocess.PIPE)
-   display = pro.stdout.read()
-   pro.wait()
-   if not badUser in display:
-      checkComplete('Removed The User '+badUser)
-
-
-def user_guest(file_path):
-   if os.path.isfile(file_path):
-     pro = subprocess.Popen("cat "+file_path, shell=True, stdout=subprocess.PIPE)
-     display = pro.stdout.read()
-     pro.wait()
-     if "allow-guest=false" in display:
-        checkComplete('Disabled Guest Account')
+###
+def user_hiddenroot(): #change: add or remove. Example: group_check('remove', 'baduser', 'sudo')
+   display = readFile('/etc/passwd')
+   for line in display.split('\n'):
+       if ':0:0:' in line and 'root' not in line:
+           return 0
    else:
-      errorWrite('Error:No guest file located at:'+file_path)
+      checkComplete('Removed Hidden Root Account')
 
+###
+def user_check(action, user): #user_check(add|remove,<username>)
+   display = readFile('/etc/passwd')
+   if 'add' in action and user in display:
+       checkComplete('Added The User '+user)
+   if 'remove' in action and not user in display:
+       checkComplete('Removed The User '+user)
 
+###
+def user_guest():
+   display = readFile('/etc/lightdm/lightdm.conf')
+   if display and "allow-guest=false" in display:
+      checkComplete('Disabled Guest Account')
+
+###
 def group_check(change,user,group): #change: add or remove. Example: group_check('remove', 'baduser', 'sudo')
-   pro = subprocess.Popen("cat /etc/group | grep \""+group+"\"", shell=True, stdout=subprocess.PIPE)
-   display = pro.stdout.read()
-   pro.wait()
-   if change == 'add' and (user in display):
-      checkComplete('Added '+user+' To The '+group+' Group')
-   if change =='remove' and (user not in display):
-      checkComplete('Removed '+user+' From the '+group+' Group')
+   display = readFile('/etc/group')
+   for line in display.split('\n'):
+       if 'add' in change:
+           if group in line and user in line:
+               checkComplete('Added '+user+' To The '+group+' Group')
+       if 'remove' in change:
+           if group in line and user not in line:
+               checkComplete('Removed '+user+' From the '+group+' Group')
 
 
 def firewall_check(): #changed from crontab -e, was this a different check?
@@ -182,6 +184,10 @@ def console_reboot(): #Per Debian Security Guide. Go ahead...read it.
       pro = subprocess.Popen("cat /etc/init/control-alt-delete.conf | grep shutdown | grep -v ^#", shell=True, stdout=subprocess.PIPE)
       display = pro.stdout.read()
       pro.wait()
+      #display.startswith('#')
+      #for line in display.split('\n')
+         #if 'shutdown' in line and line.startswith('#')
+            #completeCheck('')
       if not display:
          completeCheck('Prevented Ctrl+Alt+Del Reboot')
    else: #they deleted the file, give points
@@ -195,8 +201,6 @@ def console_userlist():
       pro.wait()
       if 'greeter-hide-users = true' in display and 'greeter-show-manual-login = true' in display:
          checkComplete('User List Hidden At Login')
-   else:
-      errorWrite('Error: No file lightdm.conf file found')
 
 
 def password_complexity(): #break these into different settings or lump them into one...
@@ -301,14 +305,14 @@ def main():
    program_remove('nmap')
    program_remove('medusa')
    program_kernel('4','8','0') 	#Ubuntu16 ships with 4.4.0, btw.
-   user_remove('BadUser1')
-   user_remove('BadUser2')
+   user_check('remove','baduser1')
+   user_check('add','cpstudent')
    user_hiddenroot()
    group_check('remove','User1','sudo') 	#add|remove user from group
    group_check('add','cpstudent','adm')
    user_passwd('cyber', '$6$FicC')
    user_passwd('jimmy', '$6$QMoj')
-   user_passwd('ben',   '$6$SkT') 
+   user_passwd('ben',   '$6$SkT')
    malware_check('.virus.py', '/home/notauser/.virus.py')
    malware_check('setup.py', '/root/Firewall/setup.py')
    firewall_check() #is this thing on?
@@ -316,7 +320,7 @@ def main():
    password_complexity()
    password_history()
    account_policy()
-   user_guest('/etc/lightdm/lightdm.conf')
+   user_guest()
    #apache_security('/etc/apache2/conf-available/myconf.conf')
    #ssh_security()
    #php_security()
@@ -330,4 +334,3 @@ def main():
 
 if __name__ == '__main__':
  main()
-
